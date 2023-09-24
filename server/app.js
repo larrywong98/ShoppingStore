c89e1874b192fe2b1ca3f0d798f87ad993e4884e
Larry Wang <larrywong9898@gmail.com>
2023-09-24 00:06:33 -0500

    update edit

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { Product } from "./models/schema.js";
+import md5 from "md5";
 
 const app = express();
 app.use(cors());
@@ -15,26 +16,66 @@ app.get("/", (req, res) => {
   res.json("Home page");
 });
 
+const generateString = () => {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  const charactersLength = characters.length;
+  for (let i = 0; i < 10; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
 app.post("/product/create", upload.single("file"), async (req, res) => {
-  console.log(req.body);
-  console.log(req.file);
+  // console.log(req.body);
+  // console.log(req.file);
+  try {
+    const newProduct = new Product({
+      id: md5(generateString()),
+      imgPath: req.body.imgPath,
+      volume: parseInt(req.body.volume),
+      category: req.body.category,
+      desp: req.body.desp,
+      price: parseFloat(req.body.price),
+      content: req.body.content,
+      timestamp: Date.now().toString().slice(0, 10),
+    });
+    await newProduct.save();
+    res.status(200).json("ok");
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
 
-  const newProduct = new Product({
-    id: "4",
-    imgPath: req.body.imgPath,
-    volume: parseInt(req.body.volume),
-    category: req.body.category,
-    desp: req.body.desp,
-    price: parseFloat(req.body.price),
-    content: req.body.content,
-    timestamp: Date.now().toString().slice(0, 10),
-  });
-  await newProduct.save();
+app.put("/product/edit/:productId", upload.single("file"), async (req, res) => {
+  // console.log(req.body);
+  // console.log(req.file);
+  // console.log(req.params.productId);
+  try {
+    let filter = { id: req.body.id };
+    let update = {
+      imgPath: req.body.imgPath,
+      volume: parseInt(req.body.volume),
+      category: req.body.category,
+      desp: req.body.desp,
+      price: parseFloat(req.body.price),
+      content: req.body.content,
+      timestamp: Date.now().toString().slice(0, 10),
+    };
+    let newDoc = await Product.findOneAndUpdate(filter, update, {
+      new: true,
+    });
+    res.status(200).json({ status: "ok" });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+
+  // console.log(newDoc);
 });
 
 app.post("/uploadImage", upload.single("file"), (req, res) => {
-  console.log(req.body);
-  console.log(req.file);
+  // console.log(req.body);
+  // console.log(req.file);
   res.json({ name: req.file.filename });
 });
 
